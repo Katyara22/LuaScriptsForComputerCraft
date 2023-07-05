b470b4aa26870716e3eefbf6d481330ffce16425
Katyaraa1 <nazarr.kacharaba@gmail.com>
2023-07-05 19:39:13 +0300

    Add config server

---
+++ apiServer/config.lua
@@ -0,0 +1,30 @@
+modemSide = "top"
+
+buttonLabel = {
+    gasoline = "Craft gasoline",
+    coinGenerator = "Start Coin",
+    coalGenerator = "Start Coal",
+    rebootComputer = "Reboot",
+    startProgramGasoline = "Program Gasoline",
+    startProgramGenerator = "Program Generator",
+}
+
+buttonModel = {
+    gasoline = {
+        isReboot = false,
+        isStartProgram = true,
+        redstoneActive = false,
+    },
+    generator = {
+        isReboot = false,
+        isStartProgram = true,
+        redstoneActiveCoinGenerator = false,
+        redstoneActiveCoalGenerator = false,
+    }
+}
+
+computerId = {
+    serverId = 7,
+    gasolineId = 6,
+    generatorId = 10,
+}
